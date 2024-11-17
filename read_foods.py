adafc375c6f1dfa42166ca01f94843f0aea74bc7
jackt2 <jtaube@students.kennesaw.edu>
2024-11-17 18:55:46 -0500

    Update read_foods.py
    annotated for clarity and minor grammar change

--- read_foods.py
+++ read_foods.py
@@ -2,9 +2,9 @@ import pandas as pd
 # accesses .csv
 foods = pd.read_csv('foods.csv')
 
-#drop columns Upper Bound and Lower Bound since meaning unclear.
+#drop columns Upper Bound and Lower Bound since meaning unclear
 foods = foods.drop(['Upper Bound', 'Lower Bound'], axis=1)
 
+#grabs only data from .csv that follows data structure for original app data
 foods_formatted = foods[['Food','Calories']]
 
-print(foods_formatted)
