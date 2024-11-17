7559be211719067ecd81e5cdf495d719621003da
jackt2 <jtaube@students.kennesaw.edu>
2024-11-17 18:01:27 -0500

    Update read_foods.py
    Dropped columns where use was unclear.

--- read_foods.py
+++ read_foods.py
@@ -2,5 +2,7 @@ import pandas as pd
 # accesses .csv
 foods = pd.read_csv('foods.csv')
 
-for headers in foods:
-    print(headers)
+
+#foods = foods.drop(['Upper Bound', 'Lower Bound'], axis=1)
+
+print(foods)
