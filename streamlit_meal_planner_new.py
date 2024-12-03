91e2684bd19b71f21376d7c8071ce0e8d856b4ae
jackt2 <jtaube@students.kennesaw.edu>
2024-12-03 15:43:15 -0500

    changed float(input()) to st.number_input()

--- streamlit_meal_planner_new.py
+++ streamlit_meal_planner_new.py
@@ -70,7 +70,7 @@ response = st.text_input("Are you trying to lose weight? (yes/no): ")
 if response == "yes":
     try:
         # Prompt for the desired weight loss goal
-        weight_loss_goal = float(input("What is your desired weight-loss goal in pounds? "))
+        weight_loss_goal = st.number_input("What is your desired weight-loss goal in pounds? ")
         print(f"Your goal is to lose {weight_loss_goal} pounds.")
     except ValueError:
         print("Please enter a valid number for the weight loss goal.")
